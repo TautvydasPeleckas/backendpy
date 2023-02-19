6b7f0968bb46ddb551eb73f46d5c30341eafe3dc
TautvydasPeleckas <tautvydas.pel@gmail.com>
2023-02-19 13:59:59 +0200

    indentation fix

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
+import os
 app = Flask(__name__)
 
-@app.route('/defuse')
-def checkCode():
-	code = request.args.get('code')
-	if code == "123":
-		return 'correct'
+@app.route('/defuse/<code>')
+def checkCode(code):
+	answers = os.environ.get('answercodes')
+	if code in "123":
+		return 'correct code - {0} and answers - {1}'.format(code,answers)
 	return 'incorrect'
