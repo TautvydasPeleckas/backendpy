db3cec285f0f9ba48213aa374b75a472f8c9eff9
Tautvydas Peleckas <tautvydas.pel@gmail.com>
2023-04-17 17:07:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/defuse/<code>')
 def checkCode(code=None):
 	answers = os.getenv('answercodes')
+	result = os.getenv('answerresult')
 	if code in answers:
-		return 'correct, MH:next'
+		return result
 	else:
 		return 'incorrect'
