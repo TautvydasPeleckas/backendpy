9f63458b110e33edc318976291d4fb910f1220e3
Tautvydas Peleckas <tautvydas.pel@gmail.com>
2023-04-17 17:34:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/defuse')
 def checkCode(code=None):
 	args = request.args
-    	code = args.get('code')
+	code = args.get('code')
 	answers = os.getenv('answercodes')
 	result = os.getenv('answerresult')
 	if code == answers:
